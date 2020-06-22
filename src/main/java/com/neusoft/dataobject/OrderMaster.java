9e0b7de9aa2576eaa8123b6eb4845a9e62ce5224
石浩博 <42900775+626sy@users.noreply.github.com>
2020-06-22 22:45:49 +0800

    jpa

---
+++ src/main/java/com/neusoft/dataobject/OrderMaster.java
@@ -0,0 +1,32 @@
+package com.neusoft.dataobject;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+
+/**
+ * @author shihaobo
+ * @date 2020/6/22 19:05
+ * dataobject 中的类即是实体类
+ * 他是与数据库中的表字段一一对应的类
+ * 写成JavaBean模式
+ */
+@Entity
+@Data
+public class OrderMaster {
+    /*订单id  主键*/
+    @Id
+    private String orderId;
+
+    /*买家名字*/
+    private String buyerName;
+
+    /*总金额*/
+    private BigDecimal orderAmount;
+
+
+}
